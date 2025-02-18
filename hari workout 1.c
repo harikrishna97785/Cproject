4f402f3d78dc633d9f6840046b1c425c50be08c1
harikrishna97785 <hari12jack77@gmail.com>
2025-02-18 15:38:27 +0530

    upload

---
+++ hari workout 1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int  main()
+{
+char a[10]="Hari";
+printf("Name:%s,a");
+int id="172";
+printf("\nid:%d",id);
+char i[20]="";
+printf("\nDept:%s",i);
+int b="403";
+printf("mark:%i\n",b);
+int f="149";
+printf("cutoff:%f\n",f);
+}
